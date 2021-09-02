3fc4b91fa37664c20e5d6e3234223050ac3f561e
tiagosathler <sathler@gmail.com>
2021-09-02 20:30:31 -0300

    Exercício 2: concluído

--- src/Pokedex.js
+++ src/Pokedex.js
@@ -2,14 +2,51 @@ import React from "react";
 import Pokemon from "./Pokemon";
 
 class Pokedex extends React.Component {
-  constructor() {
-    super()
+  constructor(props) {
+    super(props)
+    const { pokemons } = this.props;
     this.state = {
       index: 0,
+      fireBtnStyle: {
+        backgroundColor: '#e6e2d3'
+      },
+      psychicBtnStyle: {
+        backgroundColor: '#e6e2d3'
+      },
+      // pokemonsFiltered: pokemons.filter(({type}) => type === 'Fire'),
+      pokemonsFiltered: pokemons,
     }
     this.nextPokemon = this.nextPokemon.bind(this);
+    this.fireSelected = this.fireSelected.bind(this);
+    this.psychicSelected = this.psychicSelected.bind(this);    
   }
   
+  fireSelected(pokemons) {
+    this.setState({
+      pokemonsFiltered: pokemons.filter(({type}) => type === 'Fire'),
+      fireBtnStyle: {
+        backgroundColor: '#eca1a6'
+      },
+      psychicBtnStyle: {
+        backgroundColor: '#e6e2d3'
+      },
+      index: 0,
+    });
+  };
+
+  psychicSelected(pokemons) {
+    this.setState({
+      pokemonsFiltered: pokemons.filter(({type}) => type === 'Psychic'),
+      fireBtnStyle: {
+        backgroundColor: '#e6e2d3'
+      },
+      psychicBtnStyle: {
+        backgroundColor: '#eca1a6'
+      },
+      index: 0,
+    });
+  }
+
   nextPokemon(pokemons) {
     if (this.state.index !== pokemons.length - 1  ) {
       this.setState((previousState, _props) => ({
@@ -24,13 +61,28 @@ class Pokedex extends React.Component {
 
   render() {
     const { pokemons } = this.props;
+    const { index, fireBtnStyle, psychicBtnStyle, pokemonsFiltered} = this.state;
+    
     return (
-      <div className="pokedex">
-        <button onClick={() => this.nextPokemon(pokemons)}>Next</button>
-        <Pokemon 
-          key={pokemons[this.state.index].id}
-          pokemon={pokemons[this.state.index]} />        
-      </div>
+      <section>
+        <div>
+          <button onClick={() => this.nextPokemon(pokemonsFiltered)}>Next</button>
+          <button 
+            style={fireBtnStyle } 
+            onClick={() => this.fireSelected(pokemons)}>
+            Fire
+          </button>
+          <button
+            style={psychicBtnStyle}
+            onClick={() => this.psychicSelected(pokemons)}>
+            Psychic</button>
+        </div>
+        <div className="pokedex">
+          <Pokemon 
+            key={pokemonsFiltered[index].id}
+            pokemon={pokemonsFiltered[index]} />
+        </div>
+      </section>
     );
   };
 };
